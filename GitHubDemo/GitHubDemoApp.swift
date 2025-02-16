e71c2ffce54141250685b566be898e32ef9852f1
Sebastian Fernandez <sebastian_fernandez_conde@hotmail.com>
2025-02-16 19:50:36 -0400

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Sebastian Fernandez Conde on 16/2/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubDemoTests/GitHubDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoTests.swift
+//  GitHubDemoTests
+//
+//  Created by Sebastian Fernandez Conde on 16/2/25.
+//
+
+import Testing
+@testable import GitHubDemo
+
+struct GitHubDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
