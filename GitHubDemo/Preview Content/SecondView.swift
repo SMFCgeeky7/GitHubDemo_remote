4a20338298a23d1b8370a6bcb3c30bccf2358050
Sebastian Fernandez <sebastian_fernandez_conde@hotmail.com>
2025-02-16 21:23:09 -0400

    Create second view

---
+++ GitHubDemo/Preview Content/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  GitHubDemo
+//
+//  Created by Sebastian Fernandez Conde on 16/2/25.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
